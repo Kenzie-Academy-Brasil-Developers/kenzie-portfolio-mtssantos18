1197c6161ad8e32cc4e79b01e754ccd150e78bda
Matheus Santos <eumtssantos@outlook.com>
2022-08-01 08:13:02 -0300

    feat: finished my portfolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Matheus Santos",
+  githubUser: "mtssantos18",
+  linkedinUser: "mts-santos",
+  whatsappNumber: "",
 };
